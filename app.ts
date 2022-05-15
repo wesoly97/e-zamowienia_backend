91f68f828150f52052c63efe5a5e00eee10456f5
Mikołaj Wesołek <mikolajw31@wp.pl>
2022-05-15 19:33:04 +0200

    fix: remove console logs

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`server is running at localhost:${PORT}`)
     routes(app)
 })
--- utils/getTranslation.ts
+++ utils/getTranslation.ts
@@ -1,6 +1,5 @@
 import  i18n from '../middlewares/i18n.config'
 
 export function getTranslation(key: string) {
-    console.log(i18n.__('user'))
     return i18n.__(key)
 }
