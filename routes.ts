22a13f1dc4ca0df4566579631bba41985f7c747d
Mikołaj Wesołek <mikolajw31@wp.pl>
2022-06-19 19:02:52 +0200

    feat: swagger documentation for get order endpoint

--- routes.ts
+++ routes.ts
@@ -1,18 +1,22 @@
 import { Express } from 'express'
-import {createOrder, deleteOrder, getOrder, getOrders, updateOrder} from './controllers/oders/orders.controller'
+import { createOrder, deleteOrder, getOrder, getOrders, updateOrder } from './controllers/oders/orders.controller'
 import { checkValidationResult } from "./utils/checkValidationResult"
 import { checkOrderId, orderUpdateValidator, orderValidator } from "./controllers/oders/orders.validators"
+import swaggerDocumentation from "./documentation/swagger.json"
+import swaggerUi from 'swagger-ui-express'
 
 const PATHS = {
-    ORDERS: '/orders'
+    ORDERS: '/orders',
+    DOCUMENTATION: '/documentation'
 }
 
-function routes(app: Express) {
+const routes = (app: Express) => {
     app.post(PATHS.ORDERS, orderValidator, checkValidationResult, createOrder)
     app.get(PATHS.ORDERS, getOrders)
     app.get(`${PATHS.ORDERS}/:orderId`, checkOrderId, checkValidationResult, getOrder)
     app.patch(`${PATHS.ORDERS}/:orderId`, orderUpdateValidator, checkValidationResult, updateOrder)
     app.delete(`${PATHS.ORDERS}/:orderId`, checkOrderId, checkValidationResult, deleteOrder)
+    app.use(PATHS.DOCUMENTATION, swaggerUi.serve, swaggerUi.setup(swaggerDocumentation))
 }
 
 export default routes
