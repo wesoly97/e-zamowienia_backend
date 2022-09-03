9c0147bae7cc08f48aa8099404c7a4bfbc14081e
Mikołaj Wesołek <mikolajw31@wp.pl>
2022-09-03 23:50:11 +0200

    fix: displaying files with polish characters ('ł', 'Ł')

--- controllers/files/files.controller.ts
+++ controllers/files/files.controller.ts
@@ -5,8 +5,8 @@ export const getFile:RequestHandler = async (req, res) => {
 	const key = req.params.key
 	const fileObject = await getFileStream(key)
 	const fileName = key.split('__')
-	const fileNameNormalize = fileName[1].normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-
+	const fileNameNormalize = fileName[1].normalize('NFD').replace('ł','l').replace('Ł','L').replace(/[\u0300-\u036f]/g, '')
+	console.log(fileNameNormalize)
 	res.set('content-disposition',`attachment; filename=${fileNameNormalize}`)
 	fileObject.pipe(res)
 }
