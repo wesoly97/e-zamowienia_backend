3c1e5aeab5e5bb18fd0fd35395650b123c870db5
Mikołaj Wesołek <mikolajw31@wp.pl>
2022-11-04 20:44:16 +0100

    feat: preparing for  vercel deploy

--- index.ts
+++ index.ts
@@ -6,18 +6,18 @@ import cookieParser from 'cookie-parser'
 import { getServerDomain } from './utils/getServerDomain'
 import cors from 'cors'
 
-const app = express()
+const index = express()
 
 const onSuccessConnect = () => console.log('Conected to database')
 const onErrorConnect = (error: object) => console.log(error)
 
 mongoose.connect(config.mongo.url,{ retryWrites: true, w: 'majority' }).then(onSuccessConnect).catch(onErrorConnect)
 
-app.listen(config.server.port, () => {
+index.listen(config.server.port, () => {
 	console.log(`server is running at ${getServerDomain()}/documentation`)
-	app.use(cors( { credentials: true, origin: ['http://127.0.0.1:8080','http://localhost:8080'] }))
-	app.use(cookieParser())
-	app.use(express.urlencoded({ extended: false }))
-	app.use(express.json())
-	routes(app)
+	index.use(cors( { credentials: true, origin: ['http://127.0.0.1:8080','http://localhost:8080'] }))
+	index.use(cookieParser())
+	index.use(express.urlencoded({ extended: false }))
+	index.use(express.json())
+	routes(index)
 })
