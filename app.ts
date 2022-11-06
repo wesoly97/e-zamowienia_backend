9d87eb46712776f155eb788a9ef519ffa993179d
Mikołaj Wesołek <mikolajw31@wp.pl>
2022-11-06 15:45:42 +0100

    feat: adding new frontend origin to cors

--- app.ts
+++ app.ts
@@ -13,7 +13,7 @@ const onErrorConnect = (error: object) => console.log(error)
 mongoose.connect(config.mongo.url,{ retryWrites: true, w: 'majority' }).then(onSuccessConnect).catch(onErrorConnect)
 
 app.listen(config.server.port, () => {
-	app.use(cors( { credentials: true, origin: ['http://127.0.0.1:8080','http://localhost:8080'] }))
+	app.use(cors( { credentials: true, origin: ['http://127.0.0.1:8080','http://localhost:8080','https://e-zamowienia.onrender.com'] }))
 	app.use(cookieParser())
 	app.use(express.urlencoded({ extended: false }))
 	app.use(express.json())
