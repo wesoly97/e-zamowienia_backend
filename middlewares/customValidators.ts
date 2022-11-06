8982a0fd9db8ccb0f479d7808aec0a6433659491
Mikołaj Wesołek <mikolajw31@wp.pl>
2022-11-06 23:19:08 +0100

    feat: mode and category validator (checking if value is supported)

--- middlewares/customValidators.ts
+++ middlewares/customValidators.ts
@@ -2,6 +2,8 @@ import { CustomValidator } from 'express-validator'
 import { checkDocumentFormat } from '../utils/checkDocumentFormat'
 import { isCharLetterOrNumber } from '../utils/checkVariableType'
 import { MAX_FILE_SIZE } from '../constants/files'
+import { CATEGORIES_LANGUAGE_KEYS, MODES_LANGUAGE_KEYS } from '../constants/orders'
+import { getTranslation } from '../utils/getTranslation'
 
 export const isFileDocumentFormatValid: CustomValidator  = (value, { req }) => {
 	const validDocumentArray = []
@@ -30,6 +32,12 @@ export const isFirstCharNumberOrLetter: CustomValidator  = (value, { req }) => {
 		return true
 	return isCharLetterOrNumber(value[0])
 }
+
+export const isModeValid: CustomValidator  = (value, { req }) => Object.values(MODES_LANGUAGE_KEYS)
+	.map(key => getTranslation({ key })).includes(value)
+
+export const isCategoryValid: CustomValidator  = (value, { req }) => Object.values(CATEGORIES_LANGUAGE_KEYS)
+	.map(key => getTranslation({ key })).includes(value)
 
 export const isSamePasswords: CustomValidator  = (value, { req }) => value === req.body.password
 export const isDifferentPassword: CustomValidator  = (value, { req }) => value !== req.body.password
