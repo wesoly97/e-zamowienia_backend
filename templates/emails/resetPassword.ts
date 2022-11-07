24abdb9c5be721561fccd7ff7ad7c15952c77863
Mikołaj Wesołek <mikolajw31@wp.pl>
2022-11-07 18:48:55 +0100

    refactor: changing password recovery link from mail

--- templates/emails/resetPassword.ts
+++ templates/emails/resetPassword.ts
@@ -1,5 +1,5 @@
 export const resetPasswordTemplate = (token: string) => {
 	return `<h1>Witaj,</h1>
 		<h2>Kliknij przycisk, aby zresetować hasło do konta E-ZAMOWIENIA.</h2>
-		<a href="http://127.0.0.1:8080/resetowanie-hasla/${token}" target="_blank" style="background-color: blue; color: white; padding: 8px; border-radius: 8px; text-decoration: unset;">ZRESETUJ HASŁO</button>`
+		<a href="https://e-zamowienia.onrender.com/${token}" target="_blank" style="background-color: blue; color: white; padding: 8px; border-radius: 8px; text-decoration: unset;">ZRESETUJ HASŁO</button>`
 }
