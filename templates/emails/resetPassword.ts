bb5f4751427ae831c21847559768b5b524b3fde3
Mikołaj Wesołek <mikolajw31@wp.pl>
2022-11-07 19:17:43 +0100

    fix: reset password link

--- templates/emails/resetPassword.ts
+++ templates/emails/resetPassword.ts
@@ -1,5 +1,5 @@
 export const resetPasswordTemplate = (token: string) => {
 	return `<h1>Witaj,</h1>
 		<h2>Kliknij przycisk, aby zresetować hasło do konta E-ZAMOWIENIA.</h2>
-		<a href="https://e-zamowienia.onrender.com/${token}" target="_blank" style="background-color: blue; color: white; padding: 8px; border-radius: 8px; text-decoration: unset;">ZRESETUJ HASŁO</button>`
+		<a href="https://e-zamowienia.onrender.com/resetowanie-hasla/${token}" target="_blank" style="background-color: blue; color: white; padding: 8px; border-radius: 8px; text-decoration: unset;">ZRESETUJ HASŁO</button>`
 }
