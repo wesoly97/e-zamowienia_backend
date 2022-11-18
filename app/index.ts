0db27ad2c418e50bb9b3a0044a1e2d986f281a84
Mikołaj Wesołek <mikolajw31@wp.pl>
2022-11-18 17:21:12 +0100

    feat: detecting user language by cookies from request

--- app/index.ts
+++ app/index.ts
@@ -10,5 +10,12 @@ app.use(cookieParser())
 app.use(i18n.init)
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Credentials', 'true')
+	res.header('Access-Control-Allow-Origin', req.headers.origin)
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept')
+	next()
+})
 routes(app)
 
